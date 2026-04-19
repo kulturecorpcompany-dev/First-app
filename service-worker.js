5ce6c2465265957424e04c4c25e2ce50dc4fb1b8
kulturecorpcompany-dev <kulturecorp.company@gmail.com>
2026-04-20 00:11:23 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,19 @@
-const CACHE_NAME = "prefect-app-v1";
+const CACHE = "prefect-v2";
 
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json"
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
 ];
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-    .catch(() => caches.match("/index.html"))
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r || fetch(e.request))
   );
 });
